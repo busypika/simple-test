2e9ffb69fae701fe9c9215f7f6897a41f3a50938
Chris <busypika@gmail.com>
2022-09-24 21:39:45 +0800

    Simple

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Message } from '@cors-test/api-interfaces';
+import { Message } from '@simple-test/api-interfaces';
 
 @Injectable()
 export class AppService {
